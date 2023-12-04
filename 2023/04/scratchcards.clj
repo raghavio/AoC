1fe60c1eb3110700f2360a3a0be9f17810dc0094
Raghav <raghav_ftw@hotmail.com>
2023-12-05 01:30:48 +0530

    Solution for day 4

---
+++ 2023/04/scratchcards.clj
@@ -0,0 +1,44 @@
+(require '[clojure.string :as str]
+         '[clojure.set :as set])
+
+(def scratchcards ((comp str/split-lines slurp) "04/input"))
+
+(defn split-by-space 
+  [s]
+  (str/split s #" "))
+
+(defn extract-numbers
+  [card]
+  (let [[_ game-id winning-numbers numbers] (re-find #"Card\s+(\d+): (.+?) \| (.+)" card)
+        create-set (comp set (partial remove empty?) split-by-space)]
+    {:game (Integer/parseInt game-id)
+     :winning (create-set winning-numbers)
+     :numbers (create-set numbers)}))
+
+(defn add-matching-count
+  [{:keys [winning numbers] :as data}]
+  (assoc data :matching-count ((comp count set/intersection) winning numbers)))
+
+(defn cards-&-amount
+  "Returns a map of card number and its amount."
+  [data]
+  (reduce (fn [acc {:keys [game matching-count]}]
+            (let [acc* (update acc game (fnil inc 0))
+                  won (range (inc game) (+ matching-count (inc game))) ;; Copies won per card. 
+                  copies (reduce #(assoc %1 %2 (get acc* game)) {} won)]
+              (merge-with + copies acc*))) {} data))
+
+(defn points
+  [match-count]
+  (int (Math/pow 2 (dec match-count))))
+
+(comment
+  ;; Part 1
+  (apply + (map (comp points :matching-count add-matching-count extract-numbers) scratchcards))
+  
+  ;; Part 2
+  (apply + (->> scratchcards
+                (map (comp add-matching-count extract-numbers))
+                cards-&-amount
+                vals))
+  )
